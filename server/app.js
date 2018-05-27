f3b40bcd31530a37c05f77395839be38fd812b39
akisaini <akshatsaini1@gmail.com>
2018-05-26 20:27:42 -0400

    Created new project directory and made server file.

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
